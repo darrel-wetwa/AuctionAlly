c60e0dd613efd8e11e3d913a5592f05c42b6d1e8
darrel-wetwa <deearrel@gmail.com>
2024-05-14 14:38:11 +0300

    initialcommit

---
+++ app/src/main/java/com/example/wazitoecommerce/ui/theme/screens/home/HomeScreen.kt
@@ -0,0 +1,227 @@
+package com.example.wazitoecommerce.ui.theme.screens.home
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Face
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.wazitoecommerce.ui.theme.WazitoECommerceTheme
+import com.example.wazitoecommerce.ui.theme.lBlue
+import com.example.wazitoecommerce.R
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController:NavHostController){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.background5),
+                contentScale = ContentScale.FillBounds
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        var selected by remember { mutableIntStateOf(0) }
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "AuctionAlly")},
+                    colors = TopAppBarDefaults.mediumTopAppBarColors(Color.LightGray)
+                )
+                //End of topappbar
+
+                Spacer(modifier = Modifier.height(30.dp))
+
+            },
+            bottomBar = {
+                NavigationBar {
+                    bottomNavItems.forEachIndexed { index, bottomNavItem ->
+                        NavigationBarItem(
+                            selected = index == selected,
+                            onClick = {
+                                selected = index
+                                navController.navigate(bottomNavItem.route)
+                            },
+                            icon = {
+                                BadgedBox(
+                                    badge = {
+                                        if (bottomNavItem.badges != 0) {
+                                            Badge {
+                                                Text(text = bottomNavItem.badges.toString())
+                                            }
+                                        } else if (bottomNavItem.hasNews) {
+                                            Badge()
+                                        }
+                                    }
+                                ) {
+                                    Icon(imageVector =
+                                    if (index == selected)
+                                        bottomNavItem.selectedIcon
+                                    else
+                                        bottomNavItem.unselectedIcon,
+                                        contentDescription = bottomNavItem.title)
+                                }
+
+                            },
+                            label = {
+                                Text(text = bottomNavItem.title)
+                            })
+                    }
+                }
+            },
+
+
+            floatingActionButton = {
+                FloatingActionButton(onClick = { /*TODO*/ }) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.Add,
+                            contentDescription = "menu")
+                    }
+                }
+            },
+            //Content Section
+            content = @Composable{
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Column {
+
+                        Row(modifier = Modifier.padding(start = 20.dp)){
+                            Image(painter = painterResource(id = R.drawable.ally),
+                                contentDescription = "amazon",
+                                modifier = Modifier.size(80.dp))
+
+                            Column {
+                                Text(text = "AuctionAlly", fontSize = 30.sp, color = lBlue, fontFamily = FontFamily.Serif)
+                                Text(text = "Welcome to Ally", fontSize = 20.sp)
+                            }
+                            Spacer(modifier = Modifier.width(60.dp))
+
+
+                        }
+                        //end of row
+                    }
+
+
+                }
+
+            }
+
+        )
+
+    }
+}
+
+
+
+val bottomNavItems = listOf(
+    BottomNavItem(
+        title = "Home",
+        route="home",
+        selectedIcon=Icons.Filled.Home,
+        unselectedIcon=Icons.Outlined.Home,
+        hasNews = false,
+        badges=0
+    ),
+    BottomNavItem(
+        title = "Explore",
+        route = "explore",
+        selectedIcon = Icons.Filled.Search,
+        unselectedIcon = Icons.Outlined.Search,
+        hasNews = false,
+        badges = 0
+    ),
+
+
+
+    BottomNavItem(
+        title = "Login",
+        route="login",
+        selectedIcon=Icons.Filled.Person,
+        unselectedIcon=Icons.Outlined.Person,
+        hasNews = true,
+        badges=5
+    ),
+
+    BottomNavItem(
+        title = "Signup",
+        route="signup",
+        selectedIcon=Icons.Filled.Face,
+        unselectedIcon=Icons.Outlined.Face,
+        hasNews = true,
+        badges=1
+    ),
+
+
+    )
+
+
+
+data class BottomNavItem(
+    val title :String,
+    val route :String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon :ImageVector,
+    val hasNews :Boolean,
+    val badges :Int
+)
+
+
+
+@Composable
+@Preview(showBackground = true)
+fun HomeScreenPreview(){
+    WazitoECommerceTheme {
+        HomeScreen(navController = rememberNavController())
+    }
+}
